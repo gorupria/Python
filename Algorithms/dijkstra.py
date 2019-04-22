0c4622eaf81a232856d822fb2c96344153cb6e81
Purushot <battle_g5@yahoo.com>
2019-04-22 21:53:03 +0300

    distances added

---
+++ Algorithms/dijkstra.py
@@ -0,0 +1,11 @@
+#author @Purushot
+
+class Dijkstra:
+
+    def __init__(self, graph, source, sink)
+        self.graph = graph
+        self.source = source
+        self.sink = sink
+
+    def dijkstra(self):
+
